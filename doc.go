6509eb8a2d3ec81ce625eadc4642073ed724a352
valsorym <valsorym.e@gmail.com>
2022-04-22 22:57:35 +0300

    Fix doc string

--- doc.go
+++ doc.go
@@ -1,9 +1,9 @@
-// Package scs (String Case Style) module implements methods for converting
+// Package scs (String Case Style) implements methods for converting
 // string case to various case styles: camelCase, kebab-case, PascalCase and
 // snake_case.
 package scs
 
-const version = "1.0.0"
+const version = "1.0.1"
 
 // Version returns the version of the module.
 func Version() string {

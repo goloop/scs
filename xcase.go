edf96d827bbc0d8e854923c12547a7449abbf13a
goloop <goloop.one@gmail.com>
2024-12-07 23:52:24 +0200

    Optimization for StrTo*

--- xcase.go
+++ xcase.go
@@ -1,55 +1,80 @@
 package scs
 
 import (
-	"regexp"
 	"strings"
+	"unicode"
 )
-
-var chunkLayout = regexp.MustCompile(`[^0-9a-zA-Z ]+`)
 
 // The getChunks clears the string of special characters and splits the
 // string by whitespace and returns a list of words ignoring empty elements.
 func getChunks(s string) []string {
-	return strings.FieldsFunc(
-		chunkLayout.ReplaceAllString(strings.ToLower(s), " "),
-		func(c rune) bool { return c == ' ' },
-	)
-}
+	chunks := make([]string, 0, strings.Count(s, " ")+1)
+	var builder strings.Builder
+	builder.Grow(len(s))
 
-// The toUnited converts a string to a format similar to camel or PascaCase.
-func toUnited(s string, firstWordIsLower bool) string {
-	var result string
-
-	for i, chunk := range getChunks(s) {
-		// Chunk is always lowercase.
-		if i == 0 && firstWordIsLower {
-			result = chunk
-			continue
+	for _, r := range strings.ToLower(s) {
+		if unicode.IsLetter(r) || unicode.IsNumber(r) {
+			builder.WriteRune(r)
+		} else if builder.Len() > 0 {
+			chunks = append(chunks, builder.String())
+			builder.Reset()
 		}
-
-		if v, ok := abbreviations[chunk]; ok {
-			result += v
-			continue
-		}
-
-		result += strings.Title(chunk)
 	}
 
-	return result
+	if builder.Len() > 0 {
+		chunks = append(chunks, builder.String())
+	}
+
+	return chunks
+}
+
+// The toUnited converts a string to a format similar to camel or PascalCase.
+func toUnited(s string, firstWordIsLower bool) string {
+	chunks := getChunks(s)
+	if len(chunks) == 0 {
+		return ""
+	}
+
+	var builder strings.Builder
+	builder.Grow(len(s))
+
+	// Перше слово
+	if firstWordIsLower {
+		builder.WriteString(chunks[0])
+	} else if v, ok := abbreviations[chunks[0]]; ok {
+		builder.WriteString(v)
+	} else {
+		builder.WriteString(strings.Title(chunks[0]))
+	}
+
+	// Решта слів
+	for _, chunk := range chunks[1:] {
+		if v, ok := abbreviations[chunk]; ok {
+			builder.WriteString(v)
+		} else {
+			builder.WriteString(strings.Title(chunk))
+		}
+	}
+
+	return builder.String()
 }
 
 // The toSeparate converts a string to a format similar to snake or kebab-case.
 func toSeparate(s, delimiter string) string {
-	var result string
-
-	for i, chunk := range getChunks(s) {
-		if i == 0 {
-			result = chunk
-			continue
-		}
-
-		result += delimiter + chunk
+	chunks := getChunks(s)
+	if len(chunks) == 0 {
+		return ""
 	}
 
-	return result
+	var builder strings.Builder
+	totalLen := len(s) + len(chunks) - 1
+	builder.Grow(totalLen)
+
+	builder.WriteString(chunks[0])
+	for _, chunk := range chunks[1:] {
+		builder.WriteString(delimiter)
+		builder.WriteString(chunk)
+	}
+
+	return builder.String()
 }
